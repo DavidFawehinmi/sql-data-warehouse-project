c80f77a329b63fae63339c850248bad0110bec0c
DavidFawehinmi <139711500+DavidFawehinmi@users.noreply.github.com>
2025-10-22 18:56:57 -0500

    Add SQL views for customers, products, and sales
    Create views for customers, products, and sales fact.

---
+++ scripts/gold/ddl_gold.sql
@@ -0,0 +1,63 @@
+--- Create Customers Dimension dim_customers
+CREATE VIEW gold.dim_customers AS 
+
+SELECT
+	ROW_NUMBER() OVER(ORDER BY cst_id) customer_key,
+	ci.cst_id customer_id,
+	ci.cst_key customer_number,
+	ci.cst_firstname first_name,
+	ci.cst_lastname last_name,
+	lc.cntry country,
+	ci.cst_marital_status marital_status,
+	CASE 
+		WHEN ci.cst_gndr != 'n/a' THEN ci.cst_gndr
+		ELSE ca.gen
+	END gender,
+	ca.bdate birthdate,
+	ci.cst_create_date create_date
+FROM silver.crm_cust_info ci
+LEFT JOIN silver.erp_cust_az12 ca
+ON		  ci.cst_key = ca.cid
+LEFT JOIN silver.erp_loc_a101 lc
+ON		  ci.cst_key = lc.cid
+
+-- Create Products Dimension dim_products
+
+
+CREATE VIEW gold.dim_products AS 
+SELECT
+	ROW_NUMBER() OVER(ORDER BY prd_start_dt, prd_key) product_key,
+	prd_id product_id, 
+	prd_key product_number,
+	prd_nm product_name,
+	cat_id category_id,
+	pc.cat category,
+	pc.subcat subcategory,
+	pc.maintenance,
+	prd_cost product_cost,
+	prd_line product_line,
+	prd_start_dt start_date
+FROM [silver].[crm_prd_info] pn
+LEFT JOIN [silver].[erp_px_cat_g1v2] pc
+ON pn.cat_id = pc.id
+WHERE prd_end_dt IS NULL -- Filter all historical data
+
+
+-- Create Sales Fact fact_sales
+
+CREATE VIEW gold.fact_sales AS 
+SELECT
+sd.sls_ord_num order_number,
+pr.product_key,
+cu.customer_key,
+sd.sls_order_dt order_date,
+sd.sls_ship_dt shipping_date,
+sd.sls_due_dt due_date,
+sd.sls_sales sales_amount,
+sd.sls_quantity quantity,
+sd.sls_price price
+FROM silver.crm_sales_details sd 
+LEFT JOIN gold.dim_products pr
+ON sd.sls_prd_key = pr.product_number
+LEFT JOIN gold.dim_customers cu
+ON sd.sls_cust_id = cu.customer_id
